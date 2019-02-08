9239b673df1f46e4faae533df3dc04d04b963730
binbashblog <12311527+binbashblog@users.noreply.github.com>
2019-02-08 01:59:30 +0000

    Update aws_free.tf

--- aws_free.tf
+++ aws_free.tf
@@ -8,3 +8,8 @@ resource "aws_instance" "exampletest1" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
 }
+
+resource "aws_instance" "exampletest2" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+}

052ff52791ad698779ff4d2c050b616a6fb84113
root <root@Pinda-Surface.localdomain>
2019-06-10 14:28:32 +0100

    added security groups, set rdp and winrm ports to allowed to all, this need to be restricted in production.
    Will add eip and elb next and TF state files via S3

--- terraform-aws-files_Windows/windows-instance.tf
+++ terraform-aws-files_Windows/windows-instance.tf
@@ -1,12 +1,8 @@
-resource "aws_key_pair" "mykey" {
-  key_name = "mykey"
-  public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
-}
-
 resource "aws_instance" "terraform-Windows" {
-  ami = "${lookup(var.WIN_AMIS, var.aws_region)}"
-  instance_type = "t2.micro"
-  key_name = "${aws_key_pair.mykey.key_name}"
+  ami 			= "${lookup(var.WIN_AMIS, var.aws_region)}"
+  instance_type 	= "t2.micro"
+  key_name 		= "${aws_key_pair.mykey.key_name}"
+  security_groups 	= ["${aws_security_group.allow_rdp_winrm.name}"]
   user_data = <<EOF
 <powershell>
 net user ${var.INSTANCE_USERNAME} '${var.INSTANCE_PASSWORD}' /add /y
@@ -28,14 +24,39 @@ net start winrm
 EOF
 
   provisioner "file" {
-    source = "test.txt"
-    destination = "C:/test.txt"
+    source 		= "test.txt"
+    destination 	= "C:/test.txt"
   }
   connection {
-    host = "${self.public_ip}"  
-    timeout = "10m"
-    type = "winrm"
-    user = "${var.INSTANCE_USERNAME}"
-    password = "${var.INSTANCE_PASSWORD}"
+    host 		= "${self.public_ip}" # terraform has recently made a change in the windows provider that requires this 
+    timeout 		= "10m"
+    type 		= "winrm"
+    user 		= "${var.INSTANCE_USERNAME}"
+    password 		= "${var.INSTANCE_PASSWORD}"
   }
+}
+
+resource "aws_security_group" "allow_rdp_winrm" {
+  name			= "allow_rdp_winrm"
+  description		= "Allow rdp and winrm traffic"
+
+  ingress {
+    from_port		= 3389
+    to_port             = 3389
+    protocol            = "tcp"
+
+    cidr_blocks         = ["0.0.0.0/0"]  # this should be changed to something secure
+  }
+  ingress {
+    from_port           = 5985
+    to_port             = 5986
+    protocol            = "tcp"
+
+    cidr_blocks         = ["0.0.0.0/0"]  # this should be changed to something secure
+  }
+}
+
+resource "aws_key_pair" "mykey" {
+  key_name              = "mykey"
+  public_key            = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
 }
